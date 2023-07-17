0d8c3ca5903e2d202ce419dbaea46d9210f0b43b
Alif Radifan Piandy <palifradifan@gmail.com>
2023-07-17 08:54:13 +0700

    Update Seeder (Admin dan User)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Role::create([
+            'role_name' => 'admin',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Role::create([
+            'role_name' => 'user',
+        ]);
+
+        User::create([
+            'username' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin'),
+            'role_id' => 1,
+        ]);
+
+        User::create([
+            'username' => 'user1',
+            'email' => 'user1@user.com',
+            'password' => bcrypt('user'),
+            'role_id' => 2,
+        ]);
     }
 }
