bcae1b0eb3af6458c3fc15895ee7d6f15cdfa6ac
zuhriD <dikizuhri@gmail.com>
2023-07-18 20:17:26 +0700

    update view profile

--- resources/views/page/profile.blade.php
+++ resources/views/page/profile.blade.php
@@ -9,26 +9,27 @@
 <div class="container mb-5" style="margin-top: 230px">
     <h1 class="text-center" style="font-family: 'Malgun Gothic', sans-serif;font-weight: 700;">My Profile</h1>
 
-    <div class="row">
-        <div class="col-md-12 align-items-center">
-            <form class=" ">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <form>
                 <div class="mb-3">
-                  <label for="username" class="form-label">Username</label>
-                  <input type="text" class="form-control" id="username" placeholder="Enter your username">
+                    <label for="username" class="form-label" style="font-family: 'Arial Rounded MT Bold', sans-serif;font-weight: bold;">Username</label>
+                    <input type="text" class="form-control p-2" id="username"  style="background-color: #EAEAEA;box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);" readonly>
                 </div>
                 <div class="mb-3">
-                  <label for="email" class="form-label">Email</label>
-                  <input type="email" class="form-control" id="email" placeholder="Enter your email">
+                    <label for="email" class="form-label" style="font-family: 'Arial Rounded MT Bold', sans-serif;font-weight: bold;">Email</label>
+                    <input type="email" class="form-control p-2" id="email"  style="background-color: #EAEAEA;box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);" readonly>
                 </div>
                 <div class="mb-3">
-                  <label for="password" class="form-label">Password</label>
-                  <input type="password" class="form-control" id="password" placeholder="Enter your password">
+                    <label for="password" class="form-label" style="font-family: 'Arial Rounded MT Bold', sans-serif;font-weight: bold;">Password</label>
+                    <input type="password" class="form-control p-2" id="password"  style="background-color: #EAEAEA;box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);" readonly>
                 </div>
-                <button type="submit" class="btn btn-primary">Save</button>
-              </form>
+            </form>
         </div>
     </div>
     
-  </div>
+    
+</div>
+<h3 class="text-center" style="font-family: 'Josefin Sans', sans-serif;color:#6D6C6C">@FLORAMOR</h3>
 
 @endsection
