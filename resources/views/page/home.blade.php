f2a9f90361e20fa75e978ccbf3eb8dab1704710b
Alif Radifan Piandy <palifradifan@gmail.com>
2023-07-23 14:14:12 +0700

    fix home views and naming folder for seeder

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -33,117 +33,75 @@
     <div class="container">
         <h2 class="text-center mb-4 mt-5" style="font-family: 'Jacques Francois', sans-serif;">NEW</h2>
         <div class="row">
-            <div class="col-md-3">
-                <img src="{{ asset('assets/content/1.png') }}" alt="Product 1" class="img-fluid"
-                    style="font-family: 'Jacques Francois', sans-serif;">
-                <p class="text-center">Perfume Bloom Box</p>
-                <p class="text-center">Rp 1.250.000</p>
-            </div>
-            <div class="col-md-3">
-                <img src="{{ asset('assets/content/2.png') }}" alt="Product 2" class="img-fluid"
-                    style="font-family: 'Jacques Francois', sans-serif;">
-                <p class="text-center">Pink & Purple Everlasting Hot Air Balloon</p>
-                <p class="text-center">Rp 1.250.000</p>
-            </div>
-            <div class="col-md-3">
-                <img src="{{ asset('assets/content/3.png') }}" alt="Product 3" class="img-fluid"
-                    style="font-family: 'Jacques Francois', sans-serif;">
-                <p class="text-center">Rose Gold Bridal Bouquet</p>
-                <p class="text-center">Rp 1.250.000</p>
-            </div>
-            <div class="col-md-3">
-                <img src="{{ asset('assets/content/4.png') }}" alt="Product 4" class="img-fluid"
-                    style="font-family: 'Jacques Francois', sans-serif;">
-                <p class="text-center">Keepsake Pink Unicorn</p>
-                <p class="text-center">Rp 1.250.000</p>
-            </div>
+            @foreach ($productNew as $item)
+                <div class="col-md-3">
+                    <img src="{{ asset('assets/img/' . $item->image) }}" alt="Product 1" class="img-fluid"
+                        style="font-family: 'Jacques Francois', sans-serif;">
+                    <p class="text-center">{{ $item->name }}</p>
+                    <p class="text-center">Rp {{ number_format($item->price, 0, ',', '.') }}</p>
+                </div>
+            @endforeach
             <div class="text-center mt-4 mb-3">
-                <button class="btn btn-primary"
-                    style=" background-color: #FECECE;
+                <form action="{{ route('ourbrand') }}" method="get">
+                    <button class="btn btn-primary"
+                        style=" background-color: #FECECE;
         border-color: #FECECE;
         height: 50px;
         width: 200px;
         font-family: 'Jacques Francois', sans-serif;
         ">View
-                    All Products</button>
+                        All Products</button>
+                </form>
             </div>
         </div>
     </div>
     <div class="container">
         <h2 class="text-center mb-4 mt-5" style="font-family: 'Jacques Francois', sans-serif;">FRESH FLOWERS</h2>
         <div class="row">
-            <div class="col-md-3">
-                <img src="{{ asset('assets/content/5.png') }}" alt="Product 1" class="img-fluid"
-                    style="font-family: 'Jacques Francois', sans-serif;">
-                <p class="text-center">Easy Spring flower arrangements</p>
-                <p class="text-center">Rp 1.250.000</p>
-            </div>
-            <div class="col-md-3">
-                <img src="{{ asset('assets/content/6.png') }}" alt="Product 2" class="img-fluid"
-                    style="font-family: 'Jacques Francois', sans-serif;">
-                <p class="text-center">Lilium Candidium, Madona Lily</p>
-                <p class="text-center">Rp 1.250.000</p>
-            </div>
-            <div class="col-md-3">
-                <img src="{{ asset('assets/content/7.png') }}" alt="Product 3" class="img-fluid"
-                    style="font-family: 'Jacques Francois', sans-serif;">
-                <p class="text-center">Purple theme rose bouquet</p>
-                <p class="text-center">Rp 1.250.000</p>
-            </div>
-            <div class="col-md-3">
-                <img src="{{ asset('assets/content/8.png') }}" alt="Product 4" class="img-fluid"
-                    style="font-family: 'Jacques Francois', sans-serif;">
-                <p class="text-center">Tulip Fresh Flower Bouquet</p>
-                <p class="text-center">Rp 1.250.000</p>
-            </div>
+            @foreach ($productFF as $item)
+                <div class="col-md-3">
+                    <img src="{{ asset('assets/img/' . $item->image) }}" alt="Product 1" class="img-fluid"
+                        style="font-family: 'Jacques Francois', sans-serif;">
+                    <p class="text-center">{{ $item->name }}</p>
+                    <p class="text-center">Rp {{ number_format($item->price, 0, ',', '.') }}</p>
+                </div>
+            @endforeach
             <div class="text-center mt-4 mb-3">
-                <button class="btn btn-primary"
-                    style=" background-color: #FECECE;
+                <form action="{{ route('ourbrand') }}" method="get">
+                    <button class="btn btn-primary"
+                        style=" background-color: #FECECE;
         border-color: #FECECE;
         height: 50px;
         width: 200px;
         font-family: 'Jacques Francois', sans-serif;
         ">View
-                    All Products</button>
+                        All Products</button>
+                </form>
             </div>
         </div>
     </div>
     <div class="container">
-        <h2 class="text-center mb-4 mt-5" style="font-family: 'Jacques Francois', sans-serif;">ARTIFICIAL FLOWER</h2>
+        <h2 class="text-center mb-4 mt-5" style="font-family: 'Jacques Francois', sans-serif;">ARTIFICIAL FLOWERS</h2>
         <div class="row">
-            <div class="col-md-3">
-                <img src="{{ asset('assets/content/9.png') }}" alt="Product 1" class="img-fluid"
-                    style="font-family: 'Jacques Francois', sans-serif;">
-                <p class="text-center">Peorchid Romantic</p>
-                <p class="text-center">Rp 1.250.000</p>
-            </div>
-            <div class="col-md-3">
-                <img src="{{ asset('assets/content/10.png') }}" alt="Product 2" class="img-fluid"
-                    style="font-family: 'Jacques Francois', sans-serif;">
-                <p class="text-center">European Style Eucalyptus Hydrangea</p>
-                <p class="text-center">Rp 1.250.000</p>
-            </div>
-            <div class="col-md-3">
-                <img src="{{ asset('assets/content/11.png') }}" alt="Product 3" class="img-fluid"
-                    style="font-family: 'Jacques Francois', sans-serif;">
-                <p class="text-center">Hydrangea Macrophylla</p>
-                <p class="text-center">Rp 1.250.000</p>
-            </div>
-            <div class="col-md-3">
-                <img src="{{ asset('assets/content/12.png') }}" alt="Product 4" class="img-fluid"
-                    style="font-family: 'Jacques Francois', sans-serif;">
-                <p class="text-center">Tulip Artificial Flowers</p>
-                <p class="text-center">Rp 1.250.000</p>
-            </div>
+            @foreach ($productAF as $item)
+                <div class="col-md-3">
+                    <img src="{{ asset('assets/img/' . $item->image) }}" alt="Product 1" class="img-fluid"
+                        style="font-family: 'Jacques Francois', sans-serif;">
+                    <p class="text-center">{{ $item->name }}</p>
+                    <p class="text-center">Rp {{ number_format($item->price, 0, ',', '.') }}</p>
+                </div>
+            @endforeach
             <div class="text-center mt-4 mb-3">
-                <button class="btn btn-primary"
-                    style=" background-color: #FECECE;
+                <form action="{{ route('ourbrand') }}" method="get">
+                    <button class="btn btn-primary"
+                        style=" background-color: #FECECE;
         border-color: #FECECE;
         height: 50px;
         width: 200px;
         font-family: 'Jacques Francois', sans-serif;
         ">View
-                    All Products</button>
+                        All Products</button>
+                </form>
             </div>
         </div>
     </div>
@@ -177,13 +135,13 @@
             </div>
         </div>
     </div>
-    
+
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <script>
-        document.addEventListener('DOMContentLoaded', function () {
+        document.addEventListener('DOMContentLoaded', function() {
             // Check if the session has a 'success' flash message
-            @if(session('success'))
+            @if (session('success'))
                 // Show the success toast using SweetAlert2
                 Swal.fire({
                     icon: 'success',
@@ -195,7 +153,7 @@
                     timer: 3000
                 });
             @endif
-            
+
         });
     </script>
 @endsection
